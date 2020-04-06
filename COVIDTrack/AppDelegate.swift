360aac17eef4b22e93ade77452fd48cc710185f6
bibyzan <benrasmonkey@gmail.com>
2020-04-06 13:39:20 -0400

    added demo

--- COVIDTrack/AppDelegate.swift
+++ COVIDTrack/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                              @escaping (UIBackgroundFetchResult) -> Void) {
         AppManager.checkWatchedGroups() { _, count, message, _ in
             if count > 0 {
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
                 let content = UNMutableNotificationContent()
                 content.title = "You have \(count) updates"
                 content.body = message
@@ -37,6 +37,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     }
                 }
             } else {
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+                let content = UNMutableNotificationContent()
+                content.title = "You have \(count) updates"
+                content.body = message
+                content.sound = UNNotificationSound.default
+
+                let request = UNNotificationRequest(identifier: "COVID Update", content: content, trigger: trigger)
+
+                UNUserNotificationCenter.current().add(request) {(error) in
+                    if let error = error {
+                        print("Uh oh! We had an error: \(error)")
+                        completionHandler(.failed)
+                    } else {
+                        completionHandler(.newData)
+                    }
+                }
                 completionHandler(.noData)
             }
         }
